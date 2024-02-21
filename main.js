e22816dfa173137ef6b40197028cd18ac20bcbec
leticiadocrime <106694092+leticiadocrime@users.noreply.github.com>
2024-02-21 14:39:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const botoes = document.querySelectorAll(".botao");
 
+for(let i=0i <botoes.length;i++){
+  botoes[i].onclick = funcion(){
+
+    for(let j=0;j<botoes.length;j++){
+      botoes[j].classlist.remove("ativo");
+    }
+
+    botoes[i].classlist.add("ativo");
+  }
+}
